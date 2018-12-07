7c87ed320a4fc22ba9d1a8c4fc86a2d0b6dc7b29
Zach Johnson <zlg23johnson@gmail.com>
2018-12-07 11:29:12 -0600

    Cleaned up some things and finished testing.

---
+++ ProductivityManagerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductivityManagerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductivityManagerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("230a1b9d-2dfc-41a5-ba59-2d8ef5c839de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProductivityManagerTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductivityManager;
+
+namespace ProductivityManagerTests
+{
+    [TestClass]
+    public class EventsLogicTest1
+    {
+        [TestMethod]
+        public void PopulateAndUseClasses()
+        {
+
+            // school events
+            SchoolEvent s = new SchoolEvent();
+            s.EventDate = DateTime.Today;
+            s.EventName = "TestEvent";
+            s.RoomEventIsIn = "TestRoom";
+
+            Assert.AreEqual(DateTime.Today, s.EventDate);
+            Assert.AreEqual("TestEvent", s.EventName);
+            Assert.AreEqual("TestRoom", s.RoomEventIsIn);
+            
+            // work events
+            WorkEvent w = new WorkEvent();
+            w.EventDate = DateTime.Today;
+            w.EventName = "TestEvent";
+            w.MeetingTime = 0900;
+
+            Assert.AreEqual(DateTime.Today, w.EventDate);
+            Assert.AreEqual("TestEvent", w.EventName);
+            Assert.AreEqual(0900, w.MeetingTime);
+
+
+            // life events
+            LifeEvent l = new LifeEvent();
+            l.EventDate = DateTime.Today;
+            l.EventName = "TestEvent";
+            l.Location = "TestLocation";
+
+            Assert.AreEqual(DateTime.Today, l.EventDate);
+            Assert.AreEqual("TestEvent", l.EventName);
+            Assert.AreEqual("TestLocation", l.Location);
+
+        }
+    }
+}
